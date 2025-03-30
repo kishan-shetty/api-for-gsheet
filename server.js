f84bb35b0bf296f0aeeec62f04ebcfa224be540f
kishan-shetty <kishan-shetty@users.noreply.github.com>
2025-03-31 00:22:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
 const app = express();
 const PORT = process.env.PORT || 10000;
 
